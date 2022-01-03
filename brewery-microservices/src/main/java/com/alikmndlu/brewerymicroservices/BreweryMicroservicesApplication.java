f59985f7550ad587bf38350b89c25705f2c10968
Ali Kmndlu <alikmndlu1@gmail.com>
2022-01-03 19:30:04 +0330

    init project

---
+++ brewery-microservices/src/main/java/com/alikmndlu/brewerymicroservices/BreweryMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.alikmndlu.brewerymicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreweryMicroservicesApplication.class, args);
+    }
+
+}
---
+++ brewery-microservices/src/test/java/com/alikmndlu/brewerymicroservices/BreweryMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alikmndlu.brewerymicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
